f665ce0975fd8448c4c79449b20977d3dea4137e
Francis <freesix007@gmail.com>
2023-10-20 17:25:55 -0400

    Added Reusable control

--- ContactsApp/Models/ContactRepository.cs
+++ ContactsApp/Models/ContactRepository.cs
@@ -65,7 +65,5 @@ namespace ContactsApp.Models
             contacts.Add(contact);
         }
 
-
-
     }
 }
--- ContactsApp/Views/AddContactPage.xaml.cs
+++ ContactsApp/Views/AddContactPage.xaml.cs
@@ -1,3 +1,5 @@
+using ContactsApp.Models;
+
 namespace ContactsApp.Views;
 
 public partial class AddContactPage : ContentPage
@@ -7,25 +9,34 @@ public partial class AddContactPage : ContentPage
 		InitializeComponent();
 	}
 
-    private void btnCancel_Clicked(object sender, EventArgs e)
-    {
-        Shell.Current.GoToAsync("..");
+    //private void btnCancel_Clicked(object sender, EventArgs e)
+    //{
+    //    Shell.Current.GoToAsync("..");
 
-        // Shell.Current.GoToAsync($"//{nameof(ContactsPage)}");    // Same as above ("..")
-    }
+    //    //Shell.Current.GoToAsync($"//{nameof(ContactsPage)}");    // Same as above ("..")
+    //}
 
     private void contactCtrl_OnSave(object sender, EventArgs e)
     {
+        ContactRepository.AddContact(new Models.Contact
+        {
+            Name = contactCtrl.Name,
+            Email = contactCtrl.Email,
+            Address = contactCtrl.Address,
+            Phone = contactCtrl.Phone
+        });
 
+        Shell.Current.GoToAsync("..");
     }
 
     private void contactCtrl_OnCancel(object sender, EventArgs e)
     {
-
+        //Shell.Current.GoToAsync("..");   //Works the same as below
+        Shell.Current.GoToAsync($"//{nameof(ContactsPage)}");
     }
 
     private void contactCtrl_OnError(object sender, string e)
     {
-
+        DisplayAlert("Error", e, "OK");
     }
 }
